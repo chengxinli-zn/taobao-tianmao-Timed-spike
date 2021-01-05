532f5256b8445740d486d1308a5d452718d44647
chengxin li <810928813@qq.com>
2021-01-05 10:18:18 +0800

    xiaomi

--- helper.py
+++ helper.py
@@ -1,8 +1,11 @@
+import asyncio
 import datetime
+
+from pyppeteer import launch
 
 WIDTH, HEGHT = 1200, 768
 BEFORE_SECOND = 2  # 提前2秒开始循环点击
-CLICK_FREQUENCY = 0.3  # 点击频率
+CLICK_FREQUENCY = 0.1  # 点击频率
 
 
 def get_start_time(buy_time):
@@ -14,3 +17,26 @@ def get_start_time(buy_time):
     wait_second = (buy_time - datetime.datetime.now()).seconds if \
         (buy_time - datetime.datetime.now()).days >= 0 else 0
     return wait_second
+
+
+async def get_window():
+    # 开启浏览器
+    browser = await launch(headless=False)
+    content = await browser.createIncognitoBrowserContext()
+    page = await content.newPage()
+    await page.setViewport({'width': WIDTH, 'height': HEGHT})
+    return browser, page
+
+
+async def sleep_time(buy_time):
+    # 等待时间
+    wait_second = get_start_time(buy_time)
+    print(f'等待{wait_second}s')
+    if wait_second - BEFORE_SECOND > 0:
+        await asyncio.sleep(wait_second)
+
+
+async def close_window(browser):
+    # 关闭浏览器
+    await browser.close()
+
--- xiaomi.py
+++ xiaomi.py
@@ -1,28 +1,37 @@
-import asyncio
-
-from pyppeteer import launch
 from helper import *
 
 
 async def main(URL, buy_time):
-    browser = await launch(headless=False)
-    content = await browser.createIncognitoBrowserContext()
-    page = await content.newPage()
-    await page.setViewport({'width': WIDTH, 'height': HEGHT})
+    browser, page = await get_window()
+    # 30s登陆时间
     await page.goto('https://account.xiaomi.com/pass/serviceLogin?callback=http%3A%2F%2Forder.mi.com%2Flogin%2Fcallback%3Ffollowup%3Dhttps%253A%252F%252Fwww.mi.com%252F%26sign%3DNzY3MDk1YzczNmUwMGM4ODAxOWE0NjRiNTU5ZGQyMzFhYjFmOGU0Nw%2C%2C&sid=mi_eshop&_bannerBiz=mistore&_qrsize=180')
     await asyncio.sleep(30)
-    wait_second = get_start_time(buy_time)
-    if wait_second - BEFORE_SECOND > 0:
-        await asyncio.sleep(wait_second)
+
+    # 选款式时间10s
     await page.goto(URL)
+    await asyncio.sleep(10)
+
+    await sleep_time(buy_time)
+    old_url = page.url
+
+    #加入购物车
     while True:
+        index = 0
         try:
+            print(f'重试 {index}')
             # 找到“加入购物车”，点击
             await page.click('[class="btn btn-primary"]')
-
             break
         except:
+            index += 1
             await asyncio.sleep(CLICK_FREQUENCY)
+
+    # 等待页面跳转
+    while True:
+        if page.url != old_url:
+            break
+        await asyncio.sleep(CLICK_FREQUENCY)
+
     while True:
         try:
             # 找到“进入购物车”，点击
@@ -30,13 +39,11 @@ async def main(URL, buy_time):
             break
         except:
             await asyncio.sleep(CLICK_FREQUENCY)
+    # 付款
     await asyncio.sleep(100)
-    await browser.close()
+    await close_window(browser)
 
-"""
-btn btn-a btn-primary
-"""
 if __name__ == '__main__':
-    URL = input('宝贝链接：')
-    buy_time = input('请输入开售时间 【2020-02-06(空格)12:55:50】')
+    URL = input('宝贝链接：\n')
+    buy_time = input('请输入开售时间 【2020-02-06(空格)12:55:50】\n')
     asyncio.run(main(URL, buy_time))
